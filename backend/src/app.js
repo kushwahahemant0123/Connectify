8d72ba4525f49a6c539373cdb51e71182e6c73f3
Hemant Kushwaha <127169755+kushwahahemant0123@users.noreply.github.com>
2025-05-31 12:30:08 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 import { Server } from "socket.io";
 import mongoose from "mongoose";
